498aa48ec97ae3366a3c4618fcfef381eb2127ce
Kant, Krishna <KKant@cvent.com>
2020-04-19 10:48:03 +0530

    changed api urls to heroku

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
     let d = JSON.stringify({ ...data });
 
-    fetch("http://localhost:9000/bulkcreate", {
+    fetch("http://csv-to-sql-import.herokuapp.com/bulkcreate", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -66,7 +66,7 @@ class App extends Component {
   loadClickHandler = () => {
     //
     this.setState({loading:true});
-    fetch("http://localhost:9000/customers")
+    fetch("http://csv-to-sql-import.herokuapp.com/customers")
       .then((response) => {
         if (response.ok) {
           this.setState({loading:false});
@@ -94,7 +94,7 @@ deleteClickHandler = ()=>{
   this.setState({ loaded: false });
 
 
-  fetch("http://localhost:9000/customers", {
+  fetch("http://csv-to-sql-import.herokuapp.com/customers", {
     method: "DELETE"
   })
     .then((response) => {
